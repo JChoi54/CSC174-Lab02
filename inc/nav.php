ca3996c2ddea5a575d5d405159ea0795e84f035e
Joshua <hijoshuachoi@gmail.com>
2020-07-02 00:02:07 -0400

    First webpage

---
+++ inc/nav.php
@@ -0,0 +1,7 @@
+<nav class="navbar">
+    <ul>
+        <li><a href="orchestra.php">Orchestra</a></li>
+        <li><a href="2.php">2</a></li>
+        <li><a href="3.php">3</a></li>
+    </ul>
+</nav>
---
+++ orchestra.php
@@ -0,0 +1,165 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Orchestra</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/nav.css">
+</head>
+<body>
+
+<header>
+    <h1>Orchestra</h1>
+    <div>Instrumental Ensemble</div>
+</header>
+
+<?php include "inc/nav.php"; ?>
+
+<article>
+    <h2>About Orchestras</h2>
+    <figure>
+        <img src="images/orchestra.jpg" alt="">
+        <figcaption>The Jalisco Philharmonic Orchestra</figcaption>
+    </figure>
+
+    <p>
+        An orchestra is a large instrumental ensemble typical of classical music, which combines instruments from different families, including bowed string instruments such as the violin, viola, cello, and double bass, brass instruments such as the horn, trumpet, trombone and tuba, woodwinds such as the flute, oboe, clarinet and bassoon, and percussion instruments such as the timpani, bass drum, triangle, snare drum, cymbals, and mallet percussion instruments each grouped in sections. Other instruments such as the piano and celesta may sometimes appear in a fifth keyboard section or may stand alone, as may the concert harp and, for performances of some modern compositions, electronic instruments.
+    </p>
+    <p>
+        A full-size Western orchestra may sometimes be called a symphony orchestra or philharmonic orchestra (phil-, Greek for "loving" + "harmonic"). The actual number of musicians employed in a given performance may vary from seventy to over one hundred musicians, depending on the work being played and the size of the venue. A chamber orchestra (sometimes concert orchestra) is a smaller-sized ensemble of about fifty musicians or fewer. Orchestras that specialize in the Baroque music of, for example, Johann Sebastian Bach and George Frideric Handel, or Classical repertoire, such as that of Haydn and Mozart, tend to be smaller than orchestras performing a Romantic music repertoire, such as the symphonies of Johannes Brahms. The typical orchestra grew in size throughout the 18th and 19th centuries, reaching a peak with the large orchestras (of as many as 120 players) called for in the works of Richard Wagner, and later, Gustav Mahler.
+    </p>
+    <p>
+        Orchestras are usually led by a conductor who directs the performance with movements of the hands and arms, often made easier for the musicians to see by use of a conductor's baton. The conductor unifies the orchestra, sets the tempo and shapes the sound of the ensemble.[1] The conductor also prepares the orchestra by leading rehearsals before the public concert, in which the conductor provides instructions to the musicians on their interpretation of the music being performed.
+    </p>
+    <p>
+        The leader of the first violin section, commonly called the concertmaster, also plays an important role in leading the musicians. In the Baroque music era (1600–1750), orchestras were often led by the concertmaster or by a chord-playing musician performing the basso continuo parts on a harpsichord or pipe organ, a tradition that some 20th-century and 21st-century early music ensembles continue. Orchestras play a wide range of repertoire, including symphonies, opera and ballet overtures, concertos for solo instruments, and as pit ensembles for operas, ballets, and some types of musical theatre (e.g., Gilbert and Sullivan operettas).
+    </p>
+    <h2>History</h2>
+    <p>
+        The invention of the piston and rotary valve by Heinrich Stölzel and Friedrich Blühmel, both Silesians, in 1815, was the first in a series of innovations which impacted the orchestra, including the development of modern keywork for the flute by Theobald Boehm and the innovations of Adolphe Sax in the woodwinds, notably the invention of the saxophone. These advances would lead Hector Berlioz to write a landmark book on instrumentation, which was the first systematic treatise on the use of instrumental sound as an expressive element of music.
+    </p>
+    <p>
+        The next major expansion of symphonic practice came from Richard Wagner's Bayreuth orchestra, founded to accompany his musical dramas. Wagner's works for the stage were scored with unprecedented scope and complexity: indeed, his score to Das Rheingold calls for six harps. Thus, Wagner envisioned an ever-more-demanding role for the conductor of the theatre orchestra, as he elaborated in his influential work On Conducting. This brought about a revolution in orchestral composition, and set the style for orchestral performance for the next eighty years. Wagner's theories re-examined the importance of tempo, dynamics, bowing of string instruments and the role of principals in the orchestra.
+    </p>
+    <p>
+        As the early 20th century dawned, symphony orchestras were larger, better funded, and better trained than ever before; consequently, composers could compose larger and more ambitious works. The influence of Gustav Mahler was particularly innovational; in his later symphonies, such as the mammoth Symphony No. 8, Mahler pushes the furthest boundaries of orchestral size, employing huge forces. By the late Romantic era, orchestras could support the most enormous forms of symphonic expression, with huge string sections, massive brass sections and an expanded range of percussion instruments. With the recording era beginning, the standards of performance were pushed to a new level, because a recorded symphony could be listened to closely and even minor errors in intonation or ensemble, which might not be noticeable in a live performance, could be heard by critics. As recording technologies improved over the 20th and 21st centuries, eventually small errors in a recording could be "fixed" by audio editing or overdubbing. Some older conductors and composers could remember a time when simply "getting through" the music as best as possible was the standard. Combined with the wider audience made possible by recording, this led to a renewed focus on particular star conductors and on a high standard of orchestral execution.
+
+    </p>
+    <h2>Instrumentation</h2>
+    <figure>
+        <img src="images/concerthall.jpg" alt="">
+        <figcaption>Modern orchestra concert hall</figcaption>
+    </figure>
+    <p>
+        The typical symphony orchestra consists of four groups of related musical instruments called the woodwinds, brass, percussion, and strings. Other instruments such as the piano and celesta may sometimes be grouped into a fifth section such as a keyboard section or may stand alone, as may the concert harp and electric and electronic instruments. The orchestra, depending on the size, contains almost all of the standard instruments in each group.
+    </p>
+    <p>
+        In the history of the orchestra, its instrumentation has been expanded over time, often agreed to have been standardized by the classical period and Ludwig van Beethoven's influence on the classical model. In the 20th and 21st century, new repertory demands expanded the instrumentation of the orchestra, resulting in a flexible use of the classical-model instruments and newly developed electric and electronic instruments in various combinations.
+    </p>
+    <p>
+        The terms symphony orchestra and philharmonic orchestra may be used to distinguish different ensembles from the same locality, such as the London Symphony Orchestra and the London Philharmonic Orchestra. A symphony orchestra will usually have over eighty musicians on its roster, in some cases over a hundred, but the actual number of musicians employed in a particular performance may vary according to the work being played and the size of the venue.
+
+    </p>
+</article>
+
+<aside>
+    <h2>Expanded Instrumentation</h2>
+    <p>
+        the orchestra can be analyzed in five periods: the Baroque era, the Classical music period, early/mid-Romantic music era, late-Romantic/early 20th-century music and 21st-century era. The first is a Baroque orchestra (i.e., J.S. Bach, Handel, Vivaldi), which generally had a smaller number of performers, and in which one or more chord-playing instruments, the basso continuo group (e.g., harpsichord or pipe organ and assorted bass instruments to perform the bassline), played an important role; the second is a typical classical period orchestra (e.g., early Beethoven along with Mozart and Haydn), which used a smaller group of performers than a Romantic music orchestra and a fairly standardized instrumentation; the third is typical of an early/mid-Romantic era (e.g., Schubert, Berlioz, Schumann, Brahms); the fourth is a late-Romantic/early 20th-century orchestra (e.g., Wagner, Mahler, Stravinsky), to the common complement of a 2010-era modern orchestra (e.g., Adams, Barber, Aaron Copland, Glass, Penderecki).
+    </p>
+    <h3>Modern Orchestra</h3>
+    <h3>Woodwinds</h3>
+    <ul>
+        <li>2-4 Flutes</li>
+        <li>2-4 Oboes</li>
+        <li>2-4 B♭ clarinets (1–2 doubling E♭ clarinet, alto clarinet or bass clarinet)</li>
+        <li>2-4 Bassoons</li>
+        <li>1 or more saxophones</li>
+    </ul>
+    <h3>Brass</h3>
+    <ul>
+        <li>4-8 horns (double horns) in F/B♭</li>
+        <li>3–6 trumpets in B♭</li>
+        <li>3–6 trombones (1–2 bass trombones)</li>
+        <li>1-2 tubas</li>
+        <li>1 or more baritone horns/euphoniums</li>
+        <li>2-4 Wagner tubas</li>
+    </ul>
+    <h3>Percussion</h3>
+    <ul>
+        <li>4-5 timpani (one player)</li>
+        <li>snare drum</li>
+        <li>tenor drum</li>
+        <li>bass drum</li>
+        <li>cymbals</li>
+        <li>triangle</li>
+        <li>tam-tam</li>
+        <li>tambourine</li>
+        <li>wood block</li>
+        <li>glockenspiel</li>
+        <li>xylophone</li>
+        <li>vibraphone</li>
+        <li>marimba</li>
+        <li>crotales</li>
+        <li>tubular bells</li>
+        <li>mark tree</li>
+        <li>drum kit (in some works)</li>
+    </ul>
+    <h3>Keyboards</h3>
+    <ul>
+        <li>piano</li>
+        <li>pipe organ</li>
+        <li>harpsichord</li>
+        <li>accordian</li>
+        <li>celesta</li>
+        <li>1-2 harps</li>
+    </ul>
+    <h3>Strings</h3>
+    <ul>
+        <li>16 1st violins</li>
+        <li>14 2nd violins</li>
+        <li>12 violas</li>
+        <li>10 cellos</li>
+        <li>8 double basses</li>
+        <li>(1 or more classical guitars of various types)</li>
+    </ul>
+</aside>
+
+<footer>
+    <h2>Citations</h2>
+    <ul>
+        <li>Jane Perlez,
+            <a href="https://www.nytimes.com/1997/02/28/world/vienna-philharmonic-lets-women-join-in-harmony.html">
+                "Vienna Philharmonic Lets Women Join in Harmony,"
+            </a>
+            The New York Times, February 28, 1997
+        </li>
+        <li>James R. Oestrich,
+            <a href="https://www.nytimes.com/1998/02/28/arts/even-legends-adjust-to-time-and-trend-even-the-vienna.html">
+                "Even Legends Adjust To Time and Trend, Even the Vienna Philharmonic",
+            </a>
+            The New York Times, 28 February 1998
+        </li>
+        <li>Hannah Levintova,
+            <a href="https://www.motherjones.com/media/2013/09/women-conductors-gap-charts-marin-alsop-proms/">
+                "Here's Why You Seldom See Women Leading a Symphony".
+            </a>
+            Mother Jones. Retrieved 2015-12-24.
+        </li>
+
+    </ul>
+    <h2>Source</h2>
+    <ul>
+        <li>
+            <a href="https://en.wikipedia.org/wiki/Orchestra">
+                Wikipedia Orchestra
+            </a>
+        </li>
+    </ul>
+
+</footer>
+
+<?php include "inc/scripts.php"; ?>
+
+</body>
+</html>
