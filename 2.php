81245a1535934c761118b9744c654d3b4a43e008
Iris Qi Miao <67702026+irism824-NF@users.noreply.github.com>
2020-07-02 12:25:14 +0800

    Update 2.php

--- 2.php
+++ 2.php
@@ -2,15 +2,124 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>2</title>
+    <title>Agatha Christie</title>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="stylesheet" type="text/css" href="css/nav.css">
 </head>
 <body>
 
+<header>
+
+	<h1>Agatha Christie</h1>
+
+	<div>An English writer who writes detective novels</div>
+
+</header>
+
 <?php include "inc/nav.php"; ?>
 
+<article>
+
+	<h2>Life and career</h2>
+
+	<figure>
+		<img src="images/agatha.jpg" alt="Agatha Christie">
+		<figcaption>Figure 1: Agatha Christie</figcaption>
+	</figure>
+
+	<p>Christie's mother Clara was born in Dublin in 1854 to Lieutenant (later Captain) Frederick Boehmer (91st Regiment of Foot) and his second wife Mary Ann Boehmer née West. Boehmer died aged 49 of bronchitis (although biographers often claim he was killed in a riding accident) in Jersey in April 1863, leaving his widow to raise Clara and her three brothers alone on a meagre income. Two weeks after Boehmer's death, Mary's sister Margaret West married widowed dry goods merchant Nathaniel Frary Miller, a U.S. citizen. To assist Mary financially, the newlyweds agreed to foster nine-year old Clara. The family settled in Timperley, Cheshire. Margaret and Nathaniel had no children together, but Nathaniel had a seventeen-year-old son, Fred Miller, from his previous marriage. Fred was born in New York City and travelled extensively after leaving his Swiss boarding school. He and Clara eventually formed a romantic attachment and were married in St Peter's Church, Notting Hill, in April 1878.</p>
+	
+	<p>Fred and Clara's first child, Margaret Frary ("Madge"), was born in Torquay in 1879, where the couple were renting lodgings. Their second child, Louis Montant ("Monty"), was born in Morristown, New Jersey, in 1880 while they were making an extended visit to the United States. When Fred's father died in 1869, he left Clara £2000; they used this money to purchase the leasehold of a villa in Torquay named Ashfield in which to raise their family. It was here that their third and final child, Agatha, was born in 1890.</p>
+	
+	<p>Christie was raised in a household with various esoteric beliefs and, like her siblings, believed that her mother Clara was a psychic with the ability of second sight. Christie's sister Madge had been sent to Roedean School in Sussex for her education, but their mother insisted that Christie receive a home education. As a result, her parents were responsible for teaching her to read and write and to master basic arithmetic, a subject she particularly enjoyed. They also taught her music, and she learned to play both the piano and the mandolin. According to one biographer, Clara believed that Christie should not learn to read until she was eight. However, thanks to her own curiosity, Christie taught herself to read much earlier. One of the earliest known photographs of Christie depicts her as a little girl with her first dog, named George Washington by her patriotic father but which she called Tony.</p>
+
+	<h2>Writings</h2>
+
+	<figure>
+		<img src="images/pera.jpg" alt="Pera Palace Hotel">
+		<figcaption>Figure 2: Christie's room in Pera Palace Hotel</figcaption>
+	</figure>
+
+	<p>Miss Jane Marple was introduced in the short-story collection The Thirteen Problems in 1927. Although Christie states that, "Miss Marple was not in any way a picture of my grandmother; she was far more fussy and spinsterish than my grandmother ever was", her autobiography does establish a firm connection between the fictional character and Christie's maternal great-aunt Margaret Miller ("Auntie-Grannie") and her "Ealing cronies". Both Marple and Miller "always expected the worst of everyone and everything, and were, with almost frightening accuracy, usually proved right.":422 Marple appeared in 12 novels and 20 stories.</p>
+	
+	<p>During the Second World War, Christie wrote two novels, Curtain and Sleeping Murder, intended as the last cases of these two great detectives, Hercule Poirot and Miss Marple. Both books were sealed in a bank vault for over thirty years and were released for publication by Christie only at the end of her life, when she realised that she could not write any more novels. These publications came on the heels of the success of the film version of Murder on the Orient Express in 1974.</p>
+	
+	<p>Christie became increasingly tired of Poirot, much as Sir Arthur Conan Doyle had grown weary of his character Sherlock Holmes. By the end of the 1930s, Christie wrote in her diary that she was finding Poirot "insufferable", and by the 1960s she felt that he was "an egocentric creep". However, unlike Conan Doyle, Christie resisted the temptation to kill her detective off while he was still popular. She saw herself as an entertainer whose job was to produce what the public liked, and the public liked Poirot. She did marry off Poirot's companion Captain Hastings in an attempt to trim her cast commitments.</p>
+	
+	<h2>Interests and influences</h2>
+	
+	<p>Christie had a lifelong interest in archaeology. She met her second husband, Sir Max Mallowan, a distinguished archaeologist, on a trip to the excavation site at Ur in 1930. But her fame as an author far surpassed his fame in archaeology. Prior to meeting Mallowan, Christie had not had any extensive brushes with archaeology, but once the two married, they made sure to only go to sites where they could work together. Christie accompanied Mallowan on countless archaeological trips, spending 3–4 months at a time in Syria and Iraq at excavation sites at Ur, Nineveh, Tell Arpachiyah, Chagar Bazar, Tell Brak, and Nimrud. She wrote novels and short stories, but also contributed work to the archaeological sites, more specifically to the archaeological restoration and labelling of ancient exhibits, including tasks such as cleaning and conserving delicate ivory pieces, reconstructing pottery, developing photos from early excavations which later led to taking photographs of the site and its findings, and taking field notes.</p>
+	
+	<p>Christie would always pay for her own board and lodging and her travel expenses so as to not influence the funding of the archaeological excavations, and she also supported excavations as an anonymous sponsor. During their time in the Middle East, there was also a large amount of time spent travelling to and from Mallowan's sites. Their extensive travelling had a strong influence on her writing, as some type of transportation often plays a part in her murderer's schemes. The large amount of travel was reused in novels such as Murder on the Orient Express, as well as suggesting the idea of archaeology as an adventure itself.</p>
+	
+	<p>After the Second World War, she chronicled her time in Syria with fondness in Come Tell Me How You Live. Anecdotes, memories, funny episodes are strung in a rough timeline, with more emphasis on eccentric characters and lovely scenery than on factual accuracy. From 8 November 2001 to 24 March 2002, The British Museum had an exhibit named Agatha Christie and Archaeology: Mystery in Mesopotamia, which presented the life of Christie and the influences of archaeology in her life and works</p>
+
+</article>
+
+<aside> 
+	
+	<h2>Agatha’s Works</h2> 
+
+	<p>Agatha is known for her 66 detective novels and 14 short story collections, particularly those revolving around her fictional detectives Hercule Poirot and Miss Marple.</p>
+	
+	<h2>Miss Marple Series</h2>
+
+	<ol>
+		<li>The Murder at the Vicarage (1930, Novel)</li>
+		<li>The Thirteen Problems (1932, Collection)</li>
+		<li>The Regatta Mystery and Other Stories (1939, Collection)</li>
+		<li>The Body in the Library (1942, Novel)</li>
+		<li>The Moving Finger (1943, Novel)</li>
+		<li>A Murder Is Announced (1950, Novel)</li>
+		<li>Three Blind Mice and Other Stories (1950, Collection)</li>
+		<li>They Do It with Mirrors (1952, Novel)</li>
+		<li>A Pocket Full of Rye (1953, Novel)</li>
+		<li>4.50 from Paddington (1957, Novel)</li>
+		<li>The Adventure of the Christmas Pudding (1960, Collection)</li>
+		<li>Double Sin and Other Stories (1961, Collection)</li>
+		<li>The Mirror Crack'd from Side to Side (1962, Novel)</li>
+		<li>A Caribbean Mystery (1964, Novel)</li>
+		<li>At Bertram's Hotel (1965, Novel)</li>
+		<li>Nemesis (1971, Novel)</li>
+		<li>Sleeping Murder (1976, Novel)</li>
+		<li>Miss Marple's Final Cases (1979, Collection)</li>
+	</ol>
+
+	<h2>Hercule Poirot Stories</h2>
+
+	<ul>
+		<li>Murder of Roger Ackroyd</li>
+		<li>Three Act Tragedy</li>
+		<li>Lord Edgware Dies</li>
+		<li>Black Coffee</li>
+		<li>Peril at End House</li>
+		<li>The Mystery of the Blue Train</li>
+		<li>Murder on the Orient Express</li>
+		<li>The Big Four</li>
+	</ul>
+
+</aside>
+
+<footer>
+	<h2>Citations</h2>
+
+	<ul>
+		<li><a href="https://www.theguardian.com/books/2015/sep/01/and-then-there-were-none-declared-worlds-favourite-agatha-christie-novel">Flood, Alison (1 September 2015). "And Then There Were None declared world's favourite Agatha Christie novel". The Guardian. </a></li>
+		<li><a href="https://www.oxfordreference.com/view/10.1093/acref/9780195169218.001.0001/acref-9780195169218-e-0098">Reitz, C. (2006). Christie, Agatha. In The Oxford Encyclopedia of British Literature. : Oxford University Press. Retrieved 24 Oct. 2019 </a></li>
+		<li><a href="https://www.agathachristie.com/about-christie/christie-experts/john-curran-75-facts-about-christie">Curran, John. "75 facts about Christie". agathachristie.com. Agatha Christie Limited. Retrieved 21 July 2017.</a></li>	
+	</ul>
+
+
+	<h2>Sources</h2>
+
+	<ul>
+		<li><a href="https://en.wikipedia.org/wiki/Agatha_Christie">Wikipedia Agatha Christie</a></li>
+		<li><a href="https://en.wikipedia.org/wiki/Miss_Marple">Wikipedia Miss Marple</a></li>
+		<li><a href="https://en.wikipedia.org/wiki/Hercule_Poirot_in_literature">Wikipedia Hercule Poirot in literature</a></li>
+	</ul>
+</footer>
 
 <?php include "inc/scripts.php"; ?>
+
 </body>
 </html>
