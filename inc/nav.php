bc6cb10921dfa1bab032df89c7377944710c8d95
eunlimkim <ekim69@u.rochester.edu>
2020-07-02 12:05:37 -0400

    Eunlim Kim
    Eunlim Kim stuff added

--- inc/nav.php
+++ inc/nav.php
@@ -2,6 +2,7 @@
     <ul>
         <li><a href="orchestra.php">Orchestra</a></li>
         <li><a href="agatha.php">Agatha Christie</a></li>
+        <li><a href="corgi.php">Corgi</a></li>
         <li><a href="3.php">3</a></li>
     </ul>
 </nav>
